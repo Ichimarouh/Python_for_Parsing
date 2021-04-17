be7177d81cda2de52b51c6bf365801ac9e84877b
Gin <Kameshiny@gmail.com>
2021-04-17 18:11:29 +0700

    Completing Homework for Lesson VII.

--- Lesson_7/items.py
+++ Lesson_7/items.py
@@ -5,7 +5,6 @@
 
 import scrapy
 
-
 class AutoyoulaParseItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
@@ -20,7 +19,6 @@ class HHVacancyItem(scrapy.Item):
     skills = scrapy.Field()
     company_url = scrapy.Field()
 
-
 class EmployerItem(scrapy.Item):
     _id = scrapy.Field()
     url = scrapy.Field()
@@ -28,3 +26,21 @@ class EmployerItem(scrapy.Item):
     area_of_activity = scrapy.Field()
     emp_description = scrapy.Field()
     emp_vacancy_offer = scrapy.Field()
+
+class Insta(scrapy.Item):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    data = scrapy.Field()
+    img = scrapy.Field()
+
+class InstaTag(Insta):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    data = scrapy.Field()
+    img = scrapy.Field()
+
+class InstaPost(Insta):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    data = scrapy.Field()
+    img = scrapy.Field()
